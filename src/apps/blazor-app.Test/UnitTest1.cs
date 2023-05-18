3964a6831a21645bf0b107ff3a9c1a83de8a2cee
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-05-18 17:54:08 +0200

    blazor-app init

---
+++ src/apps/blazor-app.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BlazorApp.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
