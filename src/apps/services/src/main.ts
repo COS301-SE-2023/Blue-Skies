bc7589f7e29b2b65305de675b51165491368bf4d
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-05-29 13:03:59 +0200

    Added getAllusers controller

--- src/apps/services/src/main.ts
+++ src/apps/services/src/main.ts
@@ -6,6 +6,7 @@
 import express from 'express';
 import * as path from 'path';
 import router from './routes/index';
+import * as tedious from 'tedious';
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
@@ -20,3 +21,26 @@ const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
+
+const config = {
+  server: process.env.AZURE_SQL_SERVER,
+  authentication: {
+    type: 'default',
+    options: {
+      userName: process.env.AZURE_SQL_USER,
+      password: process.env.AZURE_SQL_PASSWORD,
+    },
+  },
+  options: {
+    encrypt: true,
+    database: process.env.AZURE_SQL_DB,
+  },
+};
+
+const connection = new tedious.Connection(config);
+connection.on('connect', function (err) {
+  if (err) {
+    console.log('Error', err);
+  } else console.log('Connected');
+});
+connection.connect();
