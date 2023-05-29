d9d8f6a58c7006d1bd145a4c7d757ff9db03066f
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-05-29 13:03:59 +0200

    Added getAllusers controller

--- src/apps/services/src/controllers/user/user.controller.ts
+++ src/apps/services/src/controllers/user/user.controller.ts
@@ -43,38 +43,38 @@ export default class UserController {
       }
     });
   };
-  public getUser = (req: Request, res: Response) => {
-    const { userId } = req.params;
-    const query = `SELECT * FROM [User] WHERE userId = ${userId}`;
-
-    this.connection.on('connect', (err: tedious.ConnectionError) => {
-      if (err) {
-        console.log(err);
-      } else {
-        const request = new tedious.Request(
-          query,
-          (err: tedious.RequestError, rowCount: number) => {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log(rowCount);
-            }
-          }
-        );
-
-        request.on('row', (columns: tedious.ColumnValue[]) => {
-          const user: IUser = {
-            userId: columns[0].value,
-            email: columns[1].value,
-            password: columns[2].value,
-            userRole: columns[3].value,
-          };
-
-          res.send(user);
-        });
-
-        this.connection.execSql(request);
-      }
-    });
-  };
 }
+// public getUser = (req: Request, res: Response) => {
+//     const { userId } = req.params;
+//     const query = `SELECT * FROM [User] WHERE userId = ${userId}`;
+
+//     this.connection.on('connect', (err: tedious.ConnectionError) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         const request = new tedious.Request(
+//           query,
+//           (err: tedious.RequestError, rowCount: number) => {
+//             if (err) {
+//               console.log(err);
+//             } else {
+//               console.log(rowCount);
+//             }
+//           }
+//         );
+
+//         request.on('row', (columns: tedious.ColumnValue[]) => {
+//           const user: IUser = {
+//             userId: columns[0].value,
+//             email: columns[1].value,
+//             password: columns[2].value,
+//             userRole: columns[3].value,
+//           };
+
+//           res.send(user);
+//         });
+
+//         this.connection.execSql(request);
+//       }
+//     });
+//   };
