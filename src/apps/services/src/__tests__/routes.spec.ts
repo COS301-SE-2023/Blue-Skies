143470476106e03be08555fe913fefaee0872d11
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-05-29 10:35:27 +0200

    Added user router

--- src/apps/services/src/__tests__/routes.spec.ts
+++ src/apps/services/src/__tests__/routes.spec.ts
@@ -14,4 +14,14 @@ describe('GET /', () => {
     expect(res.status).toEqual(200);
     expect(res.text).toEqual('Hello World!');
   });
+
+  it('Testing the user router', async () => {
+    const res = await request(app).get('/user');
+    console.log(res.body);
+    expect(res.header['content-type']).toEqual(
+      expect.stringContaining('application/json')
+    );
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({ message: 'Welcome to the user router!' });
+  });
 });
--- src/apps/services/src/routes/index.ts
+++ src/apps/services/src/routes/index.ts
@@ -1,7 +1,9 @@
-import {Router} from 'express';
-import {testHelloWorld} from './default'
+import { Router } from 'express';
+import { testHelloWorld } from './default';
+import { userRouter } from './user/user.router';
 const router = Router();
 
 router.get('/', testHelloWorld);
+router.use('/user', userRouter);
 
 export default router;
