081b43306ac014679d0a43ed4b979b3b89852ea5
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-06-05 10:18:08 +0200

    Added custom tailwind colors

--- src/apps/blazor-app/tailwind.config.js
+++ src/apps/blazor-app/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ['./**/*.{razor, html, cshtml}'],
   theme: {
     extend: {},
+    colors: {
+      'primary': {
+        '50': '#f2f2f2',
+        '100': '#e6e6e6',
+        '200': '#cccccc',
+        '300': '#b3b3b3',
+        '400': '#999999',
+        '500': '#808080',
+      }
+    }
   },
   plugins: [],
 }
