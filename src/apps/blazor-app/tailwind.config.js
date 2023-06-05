1cfe015647d8324be953294d80de17b8bf0b549d
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-06-06 01:20:51 +0200

    Login Page Init

--- src/apps/blazor-app/tailwind.config.js
+++ src/apps/blazor-app/tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   content: ['./**/*.{razor, html, cshtml}'],
   theme: {
-    extend: {},
-    colors: {
-      'primary': {
-        '100': '#e2e8f0',
-        '900': '#1a365d',
+    extend: {
+      colors: {
+        'primary': {
+          '100': '#e2e8f0',
+          '900': '#1a365d',
+        }
       }
-    }
+    },
   },
   plugins: [],
 }
