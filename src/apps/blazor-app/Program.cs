3c13346be21d440acc386e4b3250d35c37b524fb
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-06-07 08:56:14 +0200

    Removed boilerplate code

--- src/apps/blazor-app/Program.cs
+++ src/apps/blazor-app/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<BlobStorageService>();
 
 var app = builder.Build();
