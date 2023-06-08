402a91f84ba31a330efbc0e16393b458b34c04f2
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-08 21:00:05 +0200

    Update index.ts

--- src/apps/services/src/routes/index.ts
+++ src/apps/services/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { testHelloWorld } from './default';
 import { userRouter } from './user/user.router';
+import { authRouter } from './auth/auth.router';
 const router = Router();
 
 router.get('/', testHelloWorld);
 router.use('/users', userRouter);
+router.use('/auth', authRouter);
 
 export default router;
