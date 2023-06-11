912d8474d3c6e8812b5e4fc172cfe8a9c764312d
Wilcovw <wilcovanwyk9@gmail.com>
2023-06-11 20:27:32 +0200

    Added get and post to basic.calculation.router.ts

--- src/apps/services/src/routes/basic.calculation/basic.calculation.router.ts
+++ src/apps/services/src/routes/basic.calculation/basic.calculation.router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import BasicCalculationController from '../../controllers/basicCalculation/basic.calculation.controller';
+import bodyParser from 'body-parser';
 export const basicCalculationRouter = express.Router();
 
 basicCalculationRouter.get('/', (req, res) => {
@@ -7,3 +8,18 @@ basicCalculationRouter.get('/', (req, res) => {
     message: 'Welcome to the basic calculation router!',
   });
 });
+
+const basicCalculationController = new BasicCalculationController();
+basicCalculationRouter.post(
+  '/create',
+  bodyParser.json(),
+  basicCalculationController.createBasicCalculation
+);
+basicCalculationRouter.get(
+  '/all',
+  basicCalculationController.getAllBasicCalculations
+);
+basicCalculationRouter.get(
+  '/:basicCalculationId',
+  basicCalculationController.getBasicCalculation
+);
