988e21bda16c46e80be1a12c799ad064894925cb
Jason <u21477452@tuks.co.za>
2023-06-11 22:26:39 +0200

    Added delete report

--- src/apps/services/src/controllers/report/report.controller.ts
+++ src/apps/services/src/controllers/report/report.controller.ts
@@ -182,4 +182,48 @@ export default class ReportController {
       });
     }
   };
+
+  public deleteReport = async (req: Request, res: Response) => {
+    const { reportId } = req.params;
+
+    if (!Number.isInteger(Number(reportId))) {
+      return res.status(400).json({
+        error: 'Invalid reportId',
+        details: 'reportId must be an integer.',
+      });
+    }
+
+    try {
+      const query = `DELETE FROM [dbo].[reports] WHERE reportId = ${reportId}`;
+      const request = new tedious.Request(
+        query,
+        (err: tedious.RequestError, rowCount: number) => {
+          if (err) {
+            return res.status(400).json({
+              error: err.message,
+            });
+          } else if (rowCount === 0) {
+            return res.status(401).json({
+              error: 'Unauthorized',
+              details: 'Report does not exist.',
+            });
+          } else {
+            console.log(rowCount);
+          }
+        }
+      );
+
+      request.on('requestCompleted', () => {
+        res.status(200).json({
+          message: 'Appliance deleted successfully.',
+        });
+      });
+      conn.execSql(request);
+    } catch (error) {
+      res.status(500).json({
+        error: 'Failed to find appliance to delete.',
+        details: 'Database connection error.',
+      });
+    }
+  };
 }
