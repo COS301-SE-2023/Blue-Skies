9e72d7e84fd1db49545fda69c76d2cc3b10f0c4a
Jason <u21477452@tuks.co.za>
2023-06-11 18:46:07 +0200

    Added appliances to express API and routing to controllers

---
+++ src/apps/services/src/controllers/appliance/appliance.controller.ts
@@ -0,0 +1,5 @@
+import * as tedious from 'tedious';
+import { Request, Response } from 'express';
+import IAppliance from '../../models/appliance.interface';
+import { connection as conn } from '../../main';
+export default class SystemController {}
--- src/apps/services/src/routes/index.ts
+++ src/apps/services/src/routes/index.ts
@@ -3,11 +3,13 @@ import { testHelloWorld } from './default';
 import { userRouter } from './user/user.router';
 import { authRouter } from './auth/auth.router';
 import { systemRouter } from './system/system.router';
+import { applianceRouter } from './appliance/appliance.router';
 const router = Router();
 
 router.get('/', testHelloWorld);
 router.use('/user', userRouter);
 router.use('/auth', authRouter);
 router.use('/system', systemRouter);
+router.use('/appliance', applianceRouter);
 
 export default router;
