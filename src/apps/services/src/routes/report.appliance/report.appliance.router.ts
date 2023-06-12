ce2d051e7590c2259b006e9d1c3146583ad2bbd9
Jason <u21477452@tuks.co.za>
2023-06-12 19:46:03 +0200

    Added interface and routing for report appliance

---
+++ src/apps/services/src/routes/report.appliance/report.appliance.router.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import ApplianceController from '../../controllers/report.appliance/report.appliance.controller';
+import bodyParser from 'body-parser';
+export const reportApplianceRouter = express.Router();
+
+reportApplianceRouter.get('/', (req, res) => {
+  res.send({
+    message: 'Welcome to the report appliance router!',
+  });
+});
+
+const reportApplianceController = new ApplianceController();
