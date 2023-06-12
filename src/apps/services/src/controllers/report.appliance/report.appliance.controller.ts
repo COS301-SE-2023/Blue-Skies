0bfb9e3b7186376fe3da82e5785e0d6bc9b74da9
Jason <u21477452@tuks.co.za>
2023-06-12 19:48:30 +0200

    Added delete functionality to reportAppliance

--- src/apps/services/src/controllers/report.appliance/report.appliance.controller.ts
+++ src/apps/services/src/controllers/report.appliance/report.appliance.controller.ts
@@ -298,4 +298,136 @@ export default class ReportApplianceController {
       });
     }
   };
+
+  public deleteReportId = async (req: Request, res: Response) => {
+    const { reportId } = req.params;
+
+    if (!Number.isInteger(Number(reportId))) {
+      return res.status(400).json({
+        error: 'Invalid reportId',
+        details: 'reportId must be an integer.',
+      });
+    }
+
+    try {
+      const query = `DELETE FROM [dbo].[reportAppliances] WHERE reportId = ${reportId}`;
+      const request = new tedious.Request(
+        query,
+        (err: tedious.RequestError, rowCount: number) => {
+          if (err) {
+            return res.status(400).json({
+              error: err.message,
+            });
+          } else if (rowCount === 0) {
+            return res.status(401).json({
+              error: 'Unauthorized',
+              details: 'Report appliance does not exist.',
+            });
+          } else {
+            console.log(rowCount);
+          }
+        }
+      );
+
+      request.on('requestCompleted', () => {
+        res.status(200).json({
+          message: 'Report appliance deleted successfully.',
+        });
+      });
+      conn.execSql(request);
+    } catch (error) {
+      res.status(500).json({
+        error: 'Failed to find report appliance to delete.',
+        details: 'Database connection error.',
+      });
+    }
+  };
+
+  public deleteApplianceId = async (req: Request, res: Response) => {
+    const { applianceId } = req.params;
+
+    if (!Number.isInteger(Number(applianceId))) {
+      return res.status(400).json({
+        error: 'Invalid applianceId',
+        details: 'applianceId must be an integer.',
+      });
+    }
+
+    try {
+      const query = `DELETE FROM [dbo].[reportAppliances] WHERE applianceId = ${applianceId}`;
+      const request = new tedious.Request(
+        query,
+        (err: tedious.RequestError, rowCount: number) => {
+          if (err) {
+            return res.status(400).json({
+              error: err.message,
+            });
+          } else if (rowCount === 0) {
+            return res.status(401).json({
+              error: 'Unauthorized',
+              details: 'Report appliance does not exist.',
+            });
+          } else {
+            console.log(rowCount);
+          }
+        }
+      );
+
+      request.on('requestCompleted', () => {
+        res.status(200).json({
+          message: 'Report appliance deleted successfully.',
+        });
+      });
+      conn.execSql(request);
+    } catch (error) {
+      res.status(500).json({
+        error: 'Failed to find report appliance to delete.',
+        details: 'Database connection error.',
+      });
+    }
+  };
+
+  public deleteReportAppliance = async (req: Request, res: Response) => {
+    const { applianceId, reportId } = req.params;
+
+    if (!Number.isInteger(Number(applianceId))) {
+      return res.status(400).json({
+        error: 'Invalid applianceId or reportId',
+        details: 'applianceId and reportId must be an integer.',
+      });
+    }
+
+    try {
+      const query = `DELETE FROM [dbo].[reportAppliances] WHERE applianceId = ${applianceId} AND reportId = ${reportId}`;
+      const request = new tedious.Request(
+        query,
+        (err: tedious.RequestError, rowCount: number) => {
+          if (err) {
+            return res.status(400).json({
+              error: err.message,
+            });
+          } else if (rowCount === 0) {
+            return res.status(401).json({
+              error: 'Unauthorized',
+              details: 'Report appliance does not exist.',
+            });
+          } else {
+            console.log(rowCount);
+          }
+        }
+      );
+
+      request.on('requestCompleted', () => {
+        res.status(200).json({
+          message: 'Report appliance deleted successfully.',
+        });
+      });
+      conn.execSql(request);
+    } catch (error) {
+      res.status(500).json({
+        error: 'Failed to find report appliance to delete.',
+        details: 'Database connection error.',
+      });
+    }
+  };
 }
