2b5afbdf7eadecd2bd8af72d3d0863d595a68cb1
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-15 21:31:32 +0200

    Register Completed

--- src/apps/api/auth.cs
+++ src/apps/api/auth.cs
@@ -5,4 +5,11 @@ public class Auth
 
   public string? email { get; set; }
   public string? password { get; set; }
+  public string userRole { get; set; }
+  public Auth(string email, string password, string userRole)
+  {
+    this.email = email;
+    this.password = password;
+    this.userRole = userRole;
+  }
 }
