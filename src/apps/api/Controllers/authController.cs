de0f23b2193b6dc736b5041114442ec17cf07691
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-15 21:10:30 +0200

    Added email validation

--- src/apps/api/Controllers/authController.cs
+++ src/apps/api/Controllers/authController.cs
@@ -11,6 +11,22 @@ public class authController : ControllerBase
   [HttpPost("register", Name = "auth")]
   public async Task<Response> PostAsync(Auth user)
   {
+    //check if email and password is not null
+    if (user.email == null || user.password == null)
+    {
+      //return with error code 400
+      var ans = new Response { message = "Email or password is null", details = "Please enter email and password" };
+      return ans;
+    }
+    string email = user.email;
+    string password = user.password;
+    //check if email is valid
+    if (!email.Contains("@") || !email.Contains("."))
+    {
+      //return with error code 400
+      var ans = new Response { message = "Email is not valid", details = "Please enter a valid email" };
+      return ans;
+    }
     var client = new HttpClient();
     var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3333/api/auth/checkemail");
     var content = new StringContent("{\r\n    \"email\" : \"" + user.email + "\"\r\n}", null, "application/json");
@@ -18,9 +34,12 @@ public class authController : ControllerBase
     var response = await client.SendAsync(request);
     if (response.StatusCode == HttpStatusCode.OK)
     {
+      //return with error code 400
       var ans = new Response { message = "Email already exists", details = "Please login or use another email" };
       return ans;
     }
+
+
     return new Response { message = "User has successfuly registered", details = "Please login" };
   }
 }
