ef5b91dd5157805598ddb7362760d9b0b1e94fa0
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-16 21:00:13 +0200

    Integrated checkemail

--- src/apps/api/Controllers/authController.cs
+++ src/apps/api/Controllers/authController.cs
@@ -16,44 +16,42 @@ public class authController : ControllerBase
   [Route("register")]
   public async Task<IActionResult> Register(Auth user)
   {
-    //check if email and password is not null
-    if (user.email == null || user.password == null || user.userRole == null)
+    try
     {
-      //return with error code 400
-      var ans = new Response { message = "Email or password is null", details = "Please enter email and password" };
-      return BadRequest(ans.ToString());
-    }
-    string email = user.email;
-    string password = user.password;
-    string userRole = user.userRole;
-    //check if email is valid
-    if (!email.Contains("@") || !email.Contains("."))
-    {
-      //return with error code 400
-      var ans = new Response { message = "Email is not valid", details = "Please enter a valid email" };
-      return BadRequest(ans.ToString());
-    }
-    var client = new HttpClient();
-    var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3333/api/auth/checkemail");
-    var content = new StringContent("{\r\n    \"email\" : \"" + user.email + "\"\r\n}", null, "application/json");
-    request.Content = content;
-    var response = await client.SendAsync(request);
-    if (response.StatusCode == HttpStatusCode.OK)
-    {
-      //return with error code 400
-      var ans = new Response { message = "Email already exists", details = "Please login or use another email" };
-      // return BadRequest(ans.ToString());
-      return StatusCode(400, ans.ToString());
-    }
-    var request2 = new HttpRequestMessage(HttpMethod.Post, "http://localhost:3333/api/auth/register");
-    var content2 = new StringContent("{\r\n    \"email\" : \"" + email + "\",\r\n    \"password\" : \"" + password + "\",\r\n    \"userRole\" : " + userRole + "\r\n}", null, "application/json");
-    request2.Content = content2;
-    var response2 = await client.SendAsync(request2);
-    Console.WriteLine(response2.StatusCode);
-    Console.WriteLine(await response2.Content.ReadAsStringAsync());
+      //check if email and password is not null
+      if (user.email == null || user.password == null || user.userRole == null)
+      {
+        //return with error code 400
+        var ans = new Response { message = "Email or password is null", details = "Please enter email and password" };
+        return BadRequest(ans.ToString());
+      }
+      string email = user.email;
+      string password = user.password;
+      string userRole = user.userRole;
+      //check if email is valid
+      if (!email.Contains("@"))
+      {
+        //return with error code 400
+        var ans = new Response { message = "Email is not valid", details = "Please enter a valid email" };
+        return BadRequest(ans.ToString());
+      }
+      //check if email is already registered
+      var checkemail = await _authRepository.checkemail(email);
+      if (checkemail)
+      {
+        //return with error code 400
+        var ans = new Response { message = "Email is already registered", details = "Please enter a different email" };
+        return BadRequest(ans.ToString());
+      }
 
-    var ans2 = new Response { message = "User registered successfully", details = "Please login" };
-    return Ok(ans2.ToString());
+      var ans2 = new Response { message = "User registered successfully", details = "Please login" };
+      return Ok(ans2.ToString());
+    }
+    catch (System.Exception)
+    {
+      return StatusCode(500, "Internal Server Error. Please contact support.");
+      throw;
+    }
   }
 
 }
