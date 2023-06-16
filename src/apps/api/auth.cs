2023643a5fff484d977a1f617bd8707e58bc9223
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-16 22:21:10 +0200

    Removed userRole

--- src/apps/api/auth.cs
+++ src/apps/api/auth.cs
@@ -5,7 +5,7 @@ public class Auth
 
   public string? email { get; set; }
   public string? password { get; set; }
-  public string userRole { get; set; }
+  public string? userRole { get; set; }
   public Auth(string email, string password, string userRole)
   {
     this.email = email;
