914817179fd012461e00833e019547213f032b45
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-16 20:55:30 +0200

    Fixed checkemail

--- src/apps/api/Repository/AuthRepository.cs
+++ src/apps/api/Repository/AuthRepository.cs
@@ -6,16 +6,23 @@ public class AuthRepository
 {
   public async Task<bool> checkemail(string email)
   {
-    var client = new HttpClient();
-    var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3333/api/auth/checkemail");
-    var content = new StringContent("{\r\n    \"email\" : \"" + email + "\"\r\n}", null, "application/json");
-    request.Content = content;
-    var response = await client.SendAsync(request);
-    if (response.StatusCode == HttpStatusCode.OK)
+    try
     {
-      return true;
+      var client = new HttpClient();
+      var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3333/api/auth/checkemail");
+      var content = new StringContent("{\r\n    \"email\" : \"" + email + "\"\r\n}", null, "application/json");
+      request.Content = content;
+      var response = await client.SendAsync(request);
+      if (response.StatusCode == HttpStatusCode.OK)
+      {
+        return true;
+      }
+      return false;
     }
-    return false;
+    catch (System.Exception)
+    {
+      throw new Exception("Could not check email");
+    }
   }
 
   public async Task<bool> register(string email, string password, string userRole)
