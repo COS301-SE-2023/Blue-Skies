df1b3bcbf47627ea7b84e3ed11a76f79a48682b7
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-16 20:51:04 +0200

    Added checkemail

--- src/apps/api/Repository/AuthRepository.cs
+++ src/apps/api/Repository/AuthRepository.cs
@@ -1,7 +1,21 @@
+using System.Net;
+
 namespace Api.Repository;
 
 public class AuthRepository
 {
-
+  public async Task<bool> checkemail(string email)
+  {
+    var client = new HttpClient();
+    var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3333/api/auth/checkemail");
+    var content = new StringContent("{\r\n    \"email\" : \"" + email + "\"\r\n}", null, "application/json");
+    request.Content = content;
+    var response = await client.SendAsync(request);
+    if (response.StatusCode == HttpStatusCode.OK)
+    {
+      return true;
+    }
+    return false;
+  }
 }
 
