1faca43171ce30b73b97a4a2cea4f235a30efe9c
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-23 23:11:25 +0200

    Passwords mismatch test

---
+++ src/apps/api.Test/Auth.cs
@@ -0,0 +1,63 @@
+
+
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Api;
+using System.Net;
+
+namespace Api.BackendTests;
+
+public class AuthTests
+{
+
+  private WebApplicationFactory<Program>? webFactory { get; set; }
+
+  private HttpClient? httpClient { get; set; }
+
+  public AuthTests()
+  {
+    webFactory = new WebApplicationFactory<Program>();
+    httpClient = webFactory.CreateDefaultClient();
+  }
+
+  [Test]
+  public async Task HelloWorld()
+  {
+    var response = await httpClient.GetAsync("/");
+    var result = await response.Content.ReadAsStringAsync();
+    Assert.That(result, Is.EqualTo("Hello World"));
+  }
+
+  [Test]
+  public async Task Register_Passwords()
+  {
+    string email = "passwordsDoNotMatch@gmail.com";
+    string password = "password";
+    string repassword = "password2";
+    Auth test = new Auth(email, password, repassword);
+    var response = httpClient.PostAsync("/auth/register", new StringContent(JsonSerializer.Serialize(test), Encoding.UTF8, "application/json")).Result;
+    var result = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(result);
+    Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+    Assert.That(result, Is.EqualTo("Passwords do not match"));
+
+  }
+}
+
+
+public class Auth
+{
+
+  public string? email { get; set; }
+
+  public string? password { get; set; }
+
+  public string? repassword { get; set; }
+  public Auth(string email, string password, string repassword)
+  {
+    this.email = email;
+    this.password = password;
+    this.repassword = repassword;
+  }
+}
