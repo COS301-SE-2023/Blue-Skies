4d7008f598a3d4fce1b15ce3f32949538c8168ac
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-23 22:24:46 +0200

    Set up Integration Testing

--- src/apps/api.Test/UnitTest1.cs
+++ src/apps/api.Test/UnitTest1.cs
@@ -1,15 +1,25 @@
+
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
 namespace Api.Test;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
+  [SetUp]
+  public void Setup()
+  {
+  }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+  [Test]
+  public async Task Test1()
+  {
+
+    var webFactory = new WebApplicationFactory<Program>();
+    var httpClient = webFactory.CreateDefaultClient();
+
+    var response = await httpClient.GetAsync("/");
+    var result = await response.Content.ReadAsStringAsync();
+    Assert.AreEqual("Hello World", result);
+  }
 }
