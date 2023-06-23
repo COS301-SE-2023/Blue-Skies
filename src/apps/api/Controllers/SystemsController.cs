4cd41c4617192e0d185174e838cce6c6c24e6f39
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-23 16:12:25 +0200

    Added get All systems

--- src/apps/api/Controllers/SystemsController.cs
+++ src/apps/api/Controllers/SystemsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Api.Repository;
 namespace Api.Controllers;
 
 
@@ -6,5 +7,16 @@ namespace Api.Controllers;
 [Route("[controller]")]
 public class SystemsController : ControllerBase
 {
+  private readonly SystemsRepository _systemsRepository;
 
+  public SystemsController()
+  {
+    _systemsRepository = new SystemsRepository();
+  }
+
+  [HttpGet(Name = "all")]
+  public async Task<List<System>> GetAllSystems()
+  {
+    return await _systemsRepository.GetAllSystems();
+  }
 }
