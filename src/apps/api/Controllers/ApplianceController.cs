21fd94338a9c733075860ed308454e0cbe9a7caf
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-24 13:03:33 +0200

    Get all Systems function

--- src/apps/api/Controllers/ApplianceController.cs
+++ src/apps/api/Controllers/ApplianceController.cs
@@ -7,11 +7,24 @@ namespace Api.Controllers;
 [Route("[controller]")]
 public class ApplianceController : ControllerBase
 {
+  private readonly AppliancesRepository _appliancesRepository;
 
+  public ApplianceController()
+  {
+    _appliancesRepository = new AppliancesRepository();
+  }
   [HttpGet]
   [Route("all")]
   public async Task<IActionResult> GetAllAppliances()
   {
-    
+    try
+    {
+      var data = await _appliancesRepository.GetAllAplliances();
+      return Ok(data);
+    }
+    catch (Exception e)
+    {
+      return StatusCode(500, e.Message);
+    }
   }
 }
