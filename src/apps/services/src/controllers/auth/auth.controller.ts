4aead9e2433ede1c4949a62e2d28b2dc3cf8f73c
pseudo-jaycie <u21477452@tuks.co.za>
2023-06-24 17:45:30 +0200

    Added last logged in to user and auth

--- src/apps/services/src/controllers/auth/auth.controller.ts
+++ src/apps/services/src/controllers/auth/auth.controller.ts
@@ -7,7 +7,7 @@ export default class AuthController {
   public registerUser = (req: Request, res: Response) => {
     const { email, password, userRole } = req.body;
     const dateCreated = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    const query = `INSERT INTO [dbo].[users] (email, password, userRole, dateCreated) VALUES ('${email}', '${password}', '${userRole}', '${dateCreated}')`;
+    const query = `INSERT INTO [dbo].[users] (email, password, userRole, dateCreated, lastLoggedIn) VALUES ('${email}', '${password}', '${userRole}', '${dateCreated}', '${dateCreated}')`;
 
     try {
       const request = new tedious.Request(
@@ -76,6 +76,7 @@ export default class AuthController {
           password: columns[2].value,
           userRole: columns[3].value,
           dateCreated: columns[4].value,
+          lastLoggedIn: columns[5].value,
         };
       });
 
@@ -118,6 +119,41 @@ export default class AuthController {
       res.status(500).json({
         error: error,
         details: 'Database connection error.',
+      });
+    }
+  };
+
+  public updateloggedIn = (req: Request, res: Response) => {
+    const { userId } = req.params;
+    const currentDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    const query = `UPDATE [dbo].[users] SET lastLoggedIn = '${currentDate}' WHERE userId = ${userId}`;
+
+    try {
+      const request = new tedious.Request(
+        query,
+        (err: tedious.RequestError, rowCount: number) => {
+          if (err) {
+            return res.status(404).json({
+              error: err.message,
+            });
+          } else if (rowCount === 0) {
+            return res.status(404).json({
+              error: 'Not Found',
+              details: 'User does not exist.',
+            });
+          } else {
+            console.log(rowCount);
+            res.status(200).json({
+              message: 'User last logged in field updated successfully.',
+            });
+          }
+        }
+      );
+
+      conn.execSql(request);
+    } catch (error) {
+      res.status(500).json({
+        error: error.message,
       });
     }
   };
