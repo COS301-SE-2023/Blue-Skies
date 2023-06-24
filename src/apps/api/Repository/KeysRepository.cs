fb55613fac2519ba6dc693738474d198fbe2232a
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-24 21:41:57 +0200

    Added create key

--- src/apps/api/Repository/KeysRepository.cs
+++ src/apps/api/Repository/KeysRepository.cs
@@ -40,4 +40,44 @@ public class KeysRepository
     }
 
   }
+
+  //create key
+  public async Task<Keys> CreateKey(string owner, int remainingCalls)
+  {
+    try
+    {
+      string APIKey = Guid.NewGuid().ToString();
+      var client = new HttpClient();
+      var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:3333/api/key/create");
+      var content = new StringContent("{\r\n    \"owner\": \"" + owner + "\",\r\n    \"APIKey\" : \"" + APIKey + "\",\r\n    \"remainingCalls\" : \"" + remainingCalls + "\"\r\n}", null, "application/json");
+      request.Content = content;
+      var response = await client.SendAsync(request);
+
+      if (response.IsSuccessStatusCode)
+      {
+        var data = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(data);
+        var systems = JsonSerializer.Deserialize<Keys>(data);
+        if (systems != null)
+        {
+          return systems;
+        }
+        return new Keys();
+      }
+      else
+      {
+        //return empty list
+        Console.WriteLine("Error");
+        return new Keys();
+      }
+
+    }
+    catch (Exception e)
+    {
+      throw new Exception("Database Connection Error");
+    }
+
+  }
+
+
 }
