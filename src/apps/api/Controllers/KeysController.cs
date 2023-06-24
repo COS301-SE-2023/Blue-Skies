5ee774206257e20ed1f3493c89ecf6544f62aab1
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-24 21:26:19 +0200

    Create KeysController.cs

---
+++ src/apps/api/Controllers/KeysController.cs
@@ -0,0 +1,14 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Api.Repository;
+namespace Api.Controllers;
+
+
+
+[ApiController]
+[Route("[controller]")]
+public class KeysController : ControllerBase
+{
+
+
+}
