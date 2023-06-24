1a497e5c875897f74ce77128aea167c24060eeee
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-24 21:30:56 +0200

    Created Key class

---
+++ src/apps/api/Models/Keys.cs
@@ -0,0 +1,12 @@
+
+namespace Api;
+
+
+public class Keys
+{
+  public int keyId { get; set; }
+  public string? owner { get; set; }
+  public string? APIKey { get; set; }
+  public int remainingCalls { get; set; }
+  public int suspended { get; set; }
+}
