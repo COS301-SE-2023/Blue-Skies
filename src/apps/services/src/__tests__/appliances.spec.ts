f5c5cabc243923a8f4c5917b7456c3b4b79752b6
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-26 00:02:22 +0200

    Tested appliances

--- src/apps/services/src/__tests__/appliances.spec.ts
+++ src/apps/services/src/__tests__/appliances.spec.ts
@@ -30,6 +30,35 @@ jest.mock('../controllers/appliance/appliance.controller', () => ({
           appliances: appliances,
         });
       });
+    createAppliance = jest
+      .fn()
+      .mockImplementation((req: Request, res: Response) => {
+        const { type, powerUsage } = req.body;
+        if (type && powerUsage) {
+          return res.status(200).json({
+            message: 'Appliance created successfully.',
+          });
+        } else {
+          return res.status(500).json({
+            error: 'Appliance not created.',
+          });
+        }
+      });
+
+    deleteAppliance = jest
+      .fn()
+      .mockImplementation((req: Request, res: Response) => {
+        const { type, powerUsage } = req.body;
+        if (type && powerUsage) {
+          return res.status(200).json({
+            message: 'Appliance deleted successfully.',
+          });
+        } else {
+          return res.status(500).json({
+            error: 'Appliance not deleted.',
+          });
+        }
+      });
   },
 }));
 
@@ -77,6 +106,84 @@ describe('ApplianceController', () => {
             applianceQuantity: 1,
           },
         ],
+      });
+    });
+  });
+
+  //Create Appliance
+  describe('Create appliance', () => {
+    it('should return 200 if appliance can be created', () => {
+      mockRequest = {
+        body: {
+          type: 'Fridge',
+          powerUsage: 1000,
+        },
+      };
+      applianceController.createAppliance(
+        mockRequest as Request,
+        mockResponse as Response
+      );
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Appliance created successfully.',
+      });
+    });
+
+    it('should return 500 if appliance cannot be created', () => {
+      mockRequest = {
+        body: {
+          type: '',
+          powerUsage: 1000,
+        },
+      };
+      applianceController.createAppliance(
+        mockRequest as Request,
+        mockResponse as Response
+      );
+
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        error: 'Appliance not created.',
+      });
+    });
+  });
+
+  //Delete Appliance
+  describe('Delete appliance', () => {
+    it('should return 200 if appliance can be deleted', () => {
+      mockRequest = {
+        body: {
+          type: 'Fridge',
+          powerUsage: 1000,
+        },
+      };
+      applianceController.deleteAppliance(
+        mockRequest as Request,
+        mockResponse as Response
+      );
+
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        message: 'Appliance deleted successfully.',
+      });
+    });
+
+    it('should return 500 if appliance cannot be deleted', () => {
+      mockRequest = {
+        body: {
+          type: '',
+          powerUsage: 1000,
+        },
+      };
+      applianceController.deleteAppliance(
+        mockRequest as Request,
+        mockResponse as Response
+      );
+
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        error: 'Appliance not deleted.',
       });
     });
   });
