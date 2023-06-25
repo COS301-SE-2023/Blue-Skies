3b6bacca03b7ec5512c074fc21e3e631dbac942e
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-25 22:20:35 +0200

    Login works

--- src/apps/api/Repository/AuthRepository.cs
+++ src/apps/api/Repository/AuthRepository.cs
@@ -31,16 +31,7 @@ public class AuthRepository
     try
     {
 
-      //Hash password
-      int test = 123456789;
-      //Convert test to byte array
-      byte[] salt = BitConverter.GetBytes(test);
-      string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-      password: password!,
-      salt: salt,
-      prf: KeyDerivationPrf.HMACSHA256,
-      iterationCount: 100000,
-      numBytesRequested: 256 / 8));
+      string hashed = hashpassword(password);
       var client = new HttpClient();
       var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:3333/api/auth/register");
       var content = new StringContent("{\r\n    \"email\" : \"" + email + "\",\r\n    \"password\" : \"" + hashed + "\",\r\n    \"userRole\" : 0\r\n}", null, "application/json");
@@ -58,5 +49,48 @@ public class AuthRepository
     }
 
   }
+
+  //Login a user
+  public async Task<bool> login(string email, string password)
+  {
+    try
+    {
+      string hashed = hashpassword(password);
+      Console.WriteLine("hashed: " + hashed);
+      var client = new HttpClient();
+      var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:3333/api/auth/login");
+      var content = new StringContent("{\r\n    \"email\" : \"" + email + "\",\r\n    \"password\" : \"" + hashed + "\"\r\n}", null, "application/json");
+      request.Content = content;
+      var response = await client.SendAsync(request);
+      Console.WriteLine("response: " + response);
+      if (response.StatusCode == HttpStatusCode.OK)
+      {
+        Console.WriteLine("Login successful");
+        return true;
+      }
+      return false;
+    }
+    catch (Exception)
+    {
+      throw new Exception("Could not login user");
+    }
+  }
+
+  string hashpassword(string password)
+  {
+    //Hash password
+    int test = 123456789;
+    //Convert test to byte array
+    byte[] salt = BitConverter.GetBytes(test);
+    string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+    password: password!,
+    salt: salt,
+    prf: KeyDerivationPrf.HMACSHA256,
+    iterationCount: 100000,
+    numBytesRequested: 256 / 8));
+    return hashed;
+  }
+
 }
 
+//Create a hash password function
