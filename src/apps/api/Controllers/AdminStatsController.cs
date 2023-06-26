d826a9553888c546425c7df73f2e53306197a532
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-26 13:00:23 +0200

    Average System Size graphs added

---
+++ src/apps/api/Controllers/AdminStatsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Api.Repository;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AdminStatsController : ControllerBase
+{
+  private readonly AdminStatsRepository _adminStatsRepository;
+
+  public AdminStatsController()
+  {
+    _adminStatsRepository = new AdminStatsRepository();
+  }
+
+  [HttpGet("all")]
+  public async Task<ActionResult<List<SystemUsage>>> GetAllSystemUsage()
+  {
+    try
+    {
+      var result = await _adminStatsRepository.GetAllSystemUsage();
+      return Ok(result);
+    }
+    catch (Exception e)
+    {
+      return StatusCode(500, e.Message);
+    }
+  }
+
+}
