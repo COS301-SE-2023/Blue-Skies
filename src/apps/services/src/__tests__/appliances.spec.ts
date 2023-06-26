71e2fcc3630e73d391191db2df7faa6935e44356
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-26 20:44:44 +0200

    Tested appliances

--- src/apps/services/src/__tests__/appliances.spec.ts
+++ src/apps/services/src/__tests__/appliances.spec.ts
@@ -1,67 +1,37 @@
-import ApplianceController from '../controllers/appliance/appliance.controller';
 import { Request, Response } from 'express';
-jest.mock('../main', () => ({
-  connection: {
-    execSql: jest.fn(),
-  },
+import * as tedious from 'tedious';
+import ApplianceController from '../controllers/appliance/appliance.controller';
+
+jest.mock('../main', () => jest.fn());
+
+jest.mock('../main', () => {
+  return {
+    connection: {
+      execSql: jest.fn(),
+    },
+  };
+});
+// Mock the dependencies and modules
+jest.mock('tedious', () => ({
+  Request: jest.fn().mockImplementation((query, callback) => {
+    // Simulate a successful query with mock data
+    // Simulate a successful query with mock data
+    if (query.includes('WHERE keyId = 1')) {
+      callback(null, 1);
+    } else {
+      callback(null, 0);
+    }
+    const rowCount = 2;
+    const keys = [
+      { id: 1, name: 'Key 1' },
+      { id: 2, name: 'Key 2' },
+    ];
+    callback(null, rowCount);
+  }),
+  ColumnValue: jest.fn(),
 }));
 
-//Mock ApplianceController
-jest.mock('../controllers/appliance/appliance.controller', () => ({
-  __esModule: true,
-  default: class {
-    getAllAppliances = jest
-      .fn()
-      .mockImplementation((req: Request, res: Response) => {
-        const appliances = [
-          {
-            applianceName: 'Fridge',
-            appliancePower: 1000,
-            applianceQuantity: 1,
-          },
-          {
-            applianceName: 'Fridge',
-            appliancePower: 1000,
-            applianceQuantity: 1,
-          },
-        ];
-        res.status(200).json({
-          appliances: appliances,
-        });
-      });
-    createAppliance = jest
-      .fn()
-      .mockImplementation((req: Request, res: Response) => {
-        const { type, powerUsage } = req.body;
-        if (type && powerUsage) {
-          return res.status(200).json({
-            message: 'Appliance created successfully.',
-          });
-        } else {
-          return res.status(500).json({
-            error: 'Appliance not created.',
-          });
-        }
-      });
-
-    deleteAppliance = jest
-      .fn()
-      .mockImplementation((req: Request, res: Response) => {
-        const { type, powerUsage } = req.body;
-        if (type && powerUsage) {
-          return res.status(200).json({
-            message: 'Appliance deleted successfully.',
-          });
-        } else {
-          return res.status(500).json({
-            error: 'Appliance not deleted.',
-          });
-        }
-      });
-  },
-}));
-
-describe('ApplianceController', () => {
+describe('Test The Appliance Controller', () => {
   let applianceController: ApplianceController;
   let mockRequest: Partial<Request>;
   let mockResponse: Partial<Response>;
@@ -69,7 +39,6 @@ describe('ApplianceController', () => {
   beforeAll(() => {
     applianceController = new ApplianceController();
   });
-
   beforeEach(() => {
     mockRequest = {};
     mockResponse = {
@@ -78,111 +47,227 @@ describe('ApplianceController', () => {
     };
   });
 
-  //dummy test
-  it('should be defined', () => {
-    expect(applianceController).toBeDefined();
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  //Get All Appliances
-  describe('Get all appliances', () => {
-    it('should return 200 if appliances can be retrieved', () => {
+  describe('KeyController', () => {
+    //Get All appliances
+    it('should return all keys', () => {
+      // Create an instance of the KeyController
+      // Call the getAllKeys method with the mock request and response
       applianceController.getAllAppliances(
         mockRequest as Request,
         mockResponse as Response
       );
 
+      // Assert that the status and json methods were called with the correct values
       expect(mockResponse.status).toHaveBeenCalledWith(200);
-      expect(mockResponse.json).toHaveBeenCalledWith({
-        appliances: [
-          {
-            applianceName: 'Fridge',
-            appliancePower: 1000,
-            applianceQuantity: 1,
-          },
-          {
-            applianceName: 'Fridge',
-            appliancePower: 1000,
-            applianceQuantity: 1,
-          },
-        ],
-      });
+      expect(mockResponse.json).toHaveBeenCalledWith([]);
     });
   });
-
-  //Create Appliance
-  describe('Create appliance', () => {
-    it('should return 200 if appliance can be created', () => {
+  describe('createAppliance', () => {
+    it('should create the appliance successfully', () => {
       mockRequest = {
         body: {
-          type: 'Fridge',
-          powerUsage: 1000,
+          type: 'TV',
+          powerUsage: 100,
         },
-      };
+      } as Request;
+      // Call the createAppliance method with the mock request and response
       applianceController.createAppliance(
         mockRequest as Request,
         mockResponse as Response
       );
 
+      // Assert that the status and json methods were called with the correct values
       expect(mockResponse.status).toHaveBeenCalledWith(200);
       expect(mockResponse.json).toHaveBeenCalledWith({
         message: 'Appliance created successfully.',
       });
     });
 
-    it('should return 500 if appliance cannot be created', () => {
+    it('should handle error when creating the appliance', () => {
       mockRequest = {
         body: {
-          type: '',
-          powerUsage: 1000,
+          type: 'TV',
+          powerUsage: 100,
         },
-      };
+      } as Request;
+      // Mock the Request constructor to throw an error
+      jest.spyOn(tedious, 'Request').mockImplementationOnce(() => {
+        throw new Error('Failed to create appliance');
+      });
+
+      // Call the createAppliance method with the mock request and response
       applianceController.createAppliance(
         mockRequest as Request,
         mockResponse as Response
       );
 
+      // Assert that the status and json methods were called with the correct values
       expect(mockResponse.status).toHaveBeenCalledWith(500);
       expect(mockResponse.json).toHaveBeenCalledWith({
-        error: 'Appliance not created.',
+        error: 'Failed to create appliance',
       });
     });
   });
 
-  //Delete Appliance
-  describe('Delete appliance', () => {
-    it('should return 200 if appliance can be deleted', () => {
+  describe('getAppliance', () => {
+    it('should get the existing appliance', () => {
       mockRequest = {
-        body: {
-          type: 'Fridge',
-          powerUsage: 1000,
+        params: {
+          applianceId: '1',
         },
-      };
-      applianceController.deleteAppliance(
+      } as unknown as Request;
+      // Call the getAppliance method with the mock request and response
+      applianceController.getAppliance(
         mockRequest as Request,
         mockResponse as Response
       );
 
+      // Assert that the status and json methods were called with the correct values
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
+    });
+
+    it('should return Not Found for a non-existing appliance', () => {
+      mockRequest = {
+        params: {
+          applianceId: '1',
+        },
+      } as unknown as Request;
+      // Update the mock request to have a non-existing applianceId
+      mockRequest.params.applianceId = '999';
+
+      // Call the getAppliance method with the mock request and response
+      applianceController.getAppliance(
+        mockRequest as Request,
+        mockResponse as Response
+      );
+
+      // Assert that the status and json methods were called with the correct values
+      expect(mockResponse.status).toHaveBeenCalledWith(404);
+    });
+
+    it('should handle error when getting an appliance', () => {
+      mockRequest = {
+        params: {
+          applianceId: '1',
+        },
+      } as unknown as Request;
+      // Mock the Request constructor to throw an error
+      jest.spyOn(tedious, 'Request').mockImplementationOnce(() => {
+        throw new Error('Failed to get appliance');
+      });
+
+      // Call the getAppliance method with the mock request and response
+      applianceController.getAppliance(
+        mockRequest as Request,
+        mockResponse as Response
+      );
+
+      // Assert that the status and json methods were called with the correct values
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+    });
+  });
+
+  describe('updateAppliance', () => {
+    it('should update the existing appliance successfully', () => {
+      mockRequest = {
+        params: {
+          applianceId: '1',
+        },
+        body: {
+          type: 'TV',
+          powerUsage: 150,
+        },
+      } as unknown as Request;
+      // Call the updateAppliance method with the mock request and response
+      applianceController.updateAppliance(
+        mockRequest as Request,
+        mockResponse as Response
+      );
+
+      // Assert that the status and json methods were called with the correct values
       expect(mockResponse.status).toHaveBeenCalledWith(200);
       expect(mockResponse.json).toHaveBeenCalledWith({
-        message: 'Appliance deleted successfully.',
+        message: 'Appliance updated successfully.',
       });
     });
 
-    it('should return 500 if appliance cannot be deleted', () => {
+    it('should return Not Found for a non-existing appliance', () => {
       mockRequest = {
-        body: {
-          type: '',
-          powerUsage: 1000,
+        params: {
+          applianceId: '1',
         },
-      };
+        body: {
+          type: 'TV',
+          powerUsage: 150,
+        },
+      } as unknown as Request;
+
+      // Update the mock request to have a non-existing applianceId
+      mockRequest.params.applianceId = '999';
+
+      // Call the updateAppliance method with the mock request and response
+      applianceController.updateAppliance(
+        mockRequest as Request,
+        mockResponse as Response
+      );
+
+      // Assert that the status and json methods were called with the correct values
+      expect(mockResponse.status).toHaveBeenCalledWith(404);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        error: 'Not Found',
+        details: 'Appliance does not exist.',
+      });
+    });
+    it('should handle error when updating an appliance', () => {
+      mockRequest = {
+        params: {
+          applianceId: '1',
+        },
+        body: {
+          type: 'TV',
+          powerUsage: 150,
+        },
+      } as unknown as Request;
+      // Mock the Request constructor to throw an error
+      jest.spyOn(tedious, 'Request').mockImplementationOnce(() => {
+        throw new Error('Failed to update appliance');
+      });
+
+      // Call the updateAppliance method with the mock request and response
+      applianceController.updateAppliance(
+        mockRequest as Request,
+        mockResponse as Response
+      );
+
+      // Assert that the status and json methods were called with the correct values
+      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      expect(mockResponse.json).toHaveBeenCalledWith({
+        error: 'Failed to update appliance',
+      });
+    });
+  });
+
+  describe('deleteAppliance', () => {
+    it('should delete the existing appliance successfully', () => {
+      mockRequest = {
+        params: {
+          applianceId: '1',
+        },
+      } as unknown as Request;
+      // Call the deleteAppliance method with the mock request and response
       applianceController.deleteAppliance(
         mockRequest as Request,
         mockResponse as Response
       );
 
-      expect(mockResponse.status).toHaveBeenCalledWith(500);
+      // Assert that the status and json methods were called with the correct values
+      expect(mockResponse.status).toHaveBeenCalledWith(200);
       expect(mockResponse.json).toHaveBeenCalledWith({
-        error: 'Appliance not deleted.',
+        message: 'Appliance deleted successfully.',
       });
     });
   });
