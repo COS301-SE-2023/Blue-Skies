e22c8fb4c6ab751154cdb368fcb5c5e4772e2269
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-06-30 21:01:48 +0200

    Login fixed

--- src/apps/api/Repository/AuthRepository.cs
+++ src/apps/api/Repository/AuthRepository.cs
@@ -1,172 +1,173 @@
 using System.Net;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
-using System.Security.Cryptography;
+using System.Text.Json;
 
 namespace Api.Repository;
 
 public class AuthRepository
 {
-    public async Task<bool> updateLoggedin(int id)
+  public async Task<bool> updateLoggedin(int id)
+  {
+    try
     {
-        try
-        {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(
-                HttpMethod.Patch,
-                "http://localhost:3333/api/auth/lastLoggedin/" + id
-            );
-            var response = await client.SendAsync(request);
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                Console.WriteLine(".NET: updated last logged in");
-                return true;
-            }
+      var client = new HttpClient();
+      var request = new HttpRequestMessage(
+          HttpMethod.Patch,
+          "http://localhost:3333/api/auth/lastLoggedin/" + id
+      );
+      var response = await client.SendAsync(request);
+      if (response.StatusCode == HttpStatusCode.OK)
+      {
+        Console.WriteLine(".NET: updated last logged in");
+        return true;
+      }
 
-            Console.WriteLine(".NET: Could not update last logged in");
-            return false;
-        }
-        catch (Exception)
-        {
-            Console.WriteLine(".NET: Could not update last logged in");
-            throw new Exception("Could not check email");
-        }
+      Console.WriteLine(".NET: Could not update last logged in");
+      return false;
     }
-
-    public async Task<bool> checkemail(string email)
+    catch (Exception)
     {
-        try
-        {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(
-                HttpMethod.Get,
-                "http://localhost:3333/api/auth/checkemail"
-            );
-            var content = new StringContent(
-                "{\r\n    \"email\" : \"" + email + "\"\r\n}",
-                null,
-                "application/json"
-            );
-            request.Content = content;
-            var response = await client.SendAsync(request);
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                Console.WriteLine(".NET: checked email");
-                return true;
-            }
-
-            Console.WriteLine(".NET: Could not check email");
-            return false;
-        }
-        catch (Exception)
-        {
-            Console.WriteLine(".NET: Could not check email");
-            throw new Exception("Could not check email");
-        }
+      Console.WriteLine(".NET: Could not update last logged in");
+      throw new Exception("Could not check email");
     }
+  }
 
-    public async Task<bool> register(string email, string password)
+  public async Task<bool> checkemail(string email)
+  {
+    try
     {
-        try
-        {
-            string hashed = hashpassword(password);
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(
-                HttpMethod.Post,
-                "http://localhost:3333/api/auth/register"
-            );
-            var content = new StringContent(
-                "{\r\n    \"email\" : \""
-                    + email
-                    + "\",\r\n    \"password\" : \""
-                    + hashed
-                    + "\",\r\n    \"userRole\" : 0\r\n}",
-                null,
-                "application/json"
-            );
-            request.Content = content;
-            var response = await client.SendAsync(request);
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                Console.WriteLine(".NET: registered user");
-                return true;
-            }
+      var client = new HttpClient();
+      var request = new HttpRequestMessage(
+          HttpMethod.Get,
+          "http://localhost:3333/api/auth/checkemail"
+      );
+      var content = new StringContent(
+          "{\r\n    \"email\" : \"" + email + "\"\r\n}",
+          null,
+          "application/json"
+      );
+      request.Content = content;
+      var response = await client.SendAsync(request);
+      if (response.StatusCode == HttpStatusCode.OK)
+      {
+        Console.WriteLine(".NET: checked email");
+        return true;
+      }
 
-            Console.WriteLine(".NET: Could not register user");
-            return false;
-        }
-        catch (Exception)
-        {
-            Console.WriteLine(".NET: Could not register user");
-            throw new Exception("Could not register user");
-        }
+      Console.WriteLine(".NET: Could not check email");
+      return false;
     }
-
-    //Login a user
-    public async Task<bool> login(string email, string password)
+    catch (Exception)
     {
-        try
-        {
-            string hashed = hashpassword(password);
-            Console.WriteLine(".NET: login password hashed");
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(
-                HttpMethod.Get,
-                "http://localhost:3333/api/auth/login"
-            );
-            var content = new StringContent(
-                "{\r\n    \"email\" : \""
-                    + email
-                    + "\",\r\n    \"password\" : \""
-                    + hashed
-                    + "\"\r\n}",
-                null,
-                "application/json"
-            );
-            request.Content = content;
-            var response = await client.SendAsync(request);
-
-            var startPos = response.Content.ReadAsStringAsync().Result.IndexOf("userId") + 8;
-            var endPos = response.Content.ReadAsStringAsync().Result.IndexOf("email") - 2;
-            var res = response.Content
-                .ReadAsStringAsync()
-                .Result.Substring(startPos, (endPos - startPos));
-            var resNum = Int32.Parse(res);
-            await updateLoggedin(resNum);
-
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                Console.WriteLine(".NET: login successful");
-                return true;
-            }
-
-            Console.WriteLine(".NET: Could not login user");
-            return false;
-        }
-        catch (Exception)
-        {
-            Console.WriteLine(".NET: Could not login user");
-            throw new Exception("Could not login user");
-        }
+      Console.WriteLine(".NET: Could not check email");
+      throw new Exception("Could not check email");
     }
+  }
 
-    string hashpassword(string password)
+  public async Task<bool> register(string email, string password)
+  {
+    try
     {
-        //Hash password
-        int test = 123456789;
-        //Convert test to byte array
-        byte[] salt = BitConverter.GetBytes(test);
-        string hashed = Convert.ToBase64String(
-            KeyDerivation.Pbkdf2(
-                password: password!,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA256,
-                iterationCount: 100000,
-                numBytesRequested: 256 / 8
-            )
-        );
-        Console.WriteLine(".NET: hashed password");
-        return hashed;
+      string hashed = hashpassword(password);
+      var client = new HttpClient();
+      var request = new HttpRequestMessage(
+          HttpMethod.Post,
+          "http://localhost:3333/api/auth/register"
+      );
+      var content = new StringContent(
+          "{\r\n    \"email\" : \""
+              + email
+              + "\",\r\n    \"password\" : \""
+              + hashed
+              + "\",\r\n    \"userRole\" : 0\r\n}",
+          null,
+          "application/json"
+      );
+      request.Content = content;
+      var response = await client.SendAsync(request);
+      if (response.StatusCode == HttpStatusCode.OK)
+      {
+        Console.WriteLine(".NET: registered user");
+        return true;
+      }
+
+      Console.WriteLine(".NET: Could not register user");
+      return false;
     }
+    catch (Exception)
+    {
+      Console.WriteLine(".NET: Could not register user");
+      throw new Exception("Could not register user");
+    }
+  }
+
+  //Login a user
+  public async Task<Users> login(string email, string password)
+  {
+    try
+    {
+      string hashed = hashpassword(password);
+      Console.WriteLine(".NET: login password hashed");
+      var client = new HttpClient();
+      var request = new HttpRequestMessage(
+          HttpMethod.Get,
+          "http://localhost:3333/api/auth/login"
+      );
+      var content = new StringContent(
+          "{\r\n    \"email\" : \""
+              + email
+              + "\",\r\n    \"password\" : \""
+              + hashed
+              + "\"\r\n}",
+          null,
+          "application/json"
+      );
+      request.Content = content;
+      var response = await client.SendAsync(request);
+
+
+      if (response.StatusCode == HttpStatusCode.OK)
+      {
+        var data = await response.Content.ReadAsStringAsync();
+        Users? user = null;
+        if (data != null)
+        {
+          user = JsonSerializer.Deserialize<Users>(data);
+          if (user == null) throw new Exception("Could not login user");
+          await updateLoggedin(user.userId);
+          Console.WriteLine(".NET: login successful");
+          return user;
+        }
+      }
+
+      Console.WriteLine(".NET: Could not login user");
+      throw new Exception("Could not login user");
+    }
+    catch (Exception)
+    {
+      Console.WriteLine(".NET: Could not login user");
+      throw new Exception("Could not login user");
+    }
+  }
+
+  string hashpassword(string password)
+  {
+    //Hash password
+    int test = 123456789;
+    //Convert test to byte array
+    byte[] salt = BitConverter.GetBytes(test);
+    string hashed = Convert.ToBase64String(
+        KeyDerivation.Pbkdf2(
+            password: password!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8
+        )
+    );
+    Console.WriteLine(".NET: hashed password");
+    return hashed;
+  }
 }
 
 //Create a hash password function
