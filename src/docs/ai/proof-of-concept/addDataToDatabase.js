3fe70edb298e90d509c22952c38f59ee2c19509d
Wilcovw <wilcovanwyk9@gmail.com>
2023-07-10 20:16:57 +0200

    Update addDataToDatabase.js

--- src/docs/ai/proof-of-concept/addDataToDatabase.js
+++ src/docs/ai/proof-of-concept/addDataToDatabase.js
@@ -1,9 +1,12 @@
+console.log('starting...');
 const fs = require('fs');
 const path = require('path');
 
 const twoLevelsUpFolder = path.join(__dirname, '..', '..', '..', '..', '..');
 
 const trainingDataFolder = path.join(twoLevelsUpFolder, 'Training_Data');
+
+var allData = [];
 
 fs.readdir(trainingDataFolder, (err, files) => {
   if (err) {
@@ -13,7 +16,7 @@ fs.readdir(trainingDataFolder, (err, files) => {
 
   files.forEach(file => {
     if (file.endsWith('.tif')) {
-        // Example: 02496287_2020_01_04_21474836_47.tif
+        // Example: 02496287_2020_01_04_2147483647.tif
 
         var areaCode = file.split('_')[0];
         var year = file.split('_')[1];
@@ -22,15 +25,25 @@ fs.readdir(trainingDataFolder, (err, files) => {
         var solarRadiation = file.split('_')[4];
         var decimalPart = file.split('_')[5];
 
+        var date = new Date(year, month - 1, day);
+
         solarRadiation = solarRadiation + '.' + decimalPart;
         solarRadiation = parseFloat(solarRadiation);
         solarRadiation = solarRadiation / (24 * 60 * 60);
 
-        console.log('areaCode', areaCode);
-        console.log('year', year);
-        console.log('month', month);
-        console.log('day', day);
-        console.log('solarRadiation', solarRadiation);
+        //Add data as a json object to the allData
+        var data = {
+          "areaCode": areaCode,
+          "date": date,
+          "solarRadiation": solarRadiation,
+          "image": fs.readFileSync(path.join(trainingDataFolder, file), { encoding: 'base64' })
+        };
+        
+        allData.push(data);
+              
     }
+   
   });
+  console.log('allData', allData[0]);
 });
+
