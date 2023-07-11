079ae359dc60f9243e8995463b47fa966efafd3c
Wilcovw <wilcovanwyk9@gmail.com>
2023-07-11 19:13:03 +0200

    Add training data to database
    Added functionality to add training data to the database

--- src/docs/ai/proof-of-concept/addDataToDatabase.js
+++ src/docs/ai/proof-of-concept/addDataToDatabase.js
@@ -18,24 +18,24 @@ fs.readdir(trainingDataFolder, (err, files) => {
     if (file.endsWith('.tif')) {
         // Example: 02496287_2020_01_04_2147483647.tif
 
-        var areaCode = file.split('_')[0];
+        var areaId = file.split('_')[0];
         var year = file.split('_')[1];
         var month = file.split('_')[2];
         var day = file.split('_')[3];
-        var solarRadiation = file.split('_')[4];
+        var solarIrradiation = file.split('_')[4];
         var decimalPart = file.split('_')[5];
 
         var date = new Date(year, month - 1, day);
 
-        solarRadiation = solarRadiation + '.' + decimalPart;
-        solarRadiation = parseFloat(solarRadiation);
-        solarRadiation = solarRadiation / (24 * 60 * 60);
+        solarIrradiation = solarIrradiation + '.' + decimalPart;
+        solarIrradiation = parseFloat(solarIrradiation);
+        solarIrradiation = solarIrradiation / (24 * 60 * 60);
 
         //Add data as a json object to the allData
         var data = {
-          "areaCode": areaCode,
+          "areaId": areaId,
           "date": date,
-          "solarRadiation": solarRadiation,
+          "solarIrradiation": solarIrradiation,
           "image": fs.readFileSync(path.join(trainingDataFolder, file), { encoding: 'base64' })
         };
         
@@ -47,3 +47,60 @@ fs.readdir(trainingDataFolder, (err, files) => {
   console.log('allData', allData[0]);
 });
 
+//add data to database using express api
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const sql = require('mssql');
+
+const dbConfig = {
+  server: process.env.AZURE_SQL_SERVER,
+  user: process.env.AZURE_SQL_USER,
+  password: process.env.AZURE_SQL_PASSWORD,
+  database: process.env.AZURE_SQL_DB,
+  options: {
+    encrypt: true,
+  }
+}
+
+// Connect to the database
+sql.connect(dbConfig)
+  .then(() => {
+    console.log('Connected to the database');
+
+          // Insert only the first data object into the database
+          const firstData = allData[0];
+
+          const { areaId, date, solarIrradiation, image } = firstData;
+    
+          const request = new sql.Request();
+          const sqlQuery = `INSERT INTO trainingData (areaId, date, solarIrradiation, image) VALUES ('${areaId}', '${date.toISOString()}', '${solarIrradiation}', '${image}')`;
+    
+          request
+            .query(sqlQuery)
+            .then(() => {
+              console.log('Data inserted successfully');
+            })
+            .catch((err) => {
+              console.error('Error inserting data:', err);
+            });
+    
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+  });
+
+app.post('/addData', (req, res) => {
+  const { areaId, date, solarIrradiation, image } = req.body;
+
+  const request = new sql.Request();
+  const sqlQuery = `INSERT INTO trainingData (areaId, date, solarIrradiation, image) VALUES ('${areaId}', '${date}', '${solarIrradiation}', '${image}')`;
+
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
