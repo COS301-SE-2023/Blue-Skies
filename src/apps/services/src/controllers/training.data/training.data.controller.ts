ce01c1b6e0bfed8a903c0914ccd2ce6358633ef3
pseudo-jaycie <u21477452@tuks.co.za>
2023-07-19 16:52:20 +0200

    Added get Training Data by Area id

--- src/apps/services/src/controllers/training.data/training.data.controller.ts
+++ src/apps/services/src/controllers/training.data/training.data.controller.ts
@@ -77,6 +77,50 @@ export default class TrainingDataController {
     }
   };
 
+  public getTrainingDataByArea = (req: Request, res: Response) => {
+    const { areaId } = req.params;
+    const query = `SELECT * FROM [dbo].[trainingData] WHERE areaId = ${areaId}`;
+    const trainingDataArr: ITrainingData[] = [];
+
+    try {
+      const request = new tedious.Request(
+        query,
+        (err: tedious.RequestError, rowCount: number) => {
+          if (err) {
+            return res.status(400).json({
+              error: err.message,
+            });
+          } else if (rowCount === 0) {
+            return res.status(404).json({
+              error: 'Not Found',
+              details: 'No training data exists.',
+            });
+          } else {
+            console.log(rowCount);
+            res.status(200).json(trainingDataArr);
+          }
+        }
+      );
+
+      request.on('row', (columns: tedious.ColumnValue[]) => {
+        const trainingData: ITrainingData = {
+          trainingDataId: columns[0].value,
+          solarIrradiation: columns[1].value,
+          image: columns[2].value,
+          areaId: columns[3].value,
+          date: columns[4].value,
+        };
+        trainingDataArr.push(trainingData);
+      });
+
+      conn.execSql(request);
+    } catch (error) {
+      res.status(500).json({
+        error: error.message,
+      });
+    }
+  };
+
   public getTrainingData = (req: Request, res: Response) => {
     const { trainingDataId } = req.params;
 
