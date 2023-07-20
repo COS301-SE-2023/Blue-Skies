8d10ff9f152737dcf3cf934b28650daf5097b3de
Rukie007 <ruan@ufcons.com>
2023-07-20 16:54:08 +0200

    Added python scripts into API

--- src/apps/api/Controllers/SolarScore.cs
+++ src/apps/api/Controllers/SolarScore.cs
@@ -2,6 +2,7 @@ using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Api.Repository;
 using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
 
 namespace Api.Controllers;
 
@@ -15,15 +16,15 @@ public class SolarScoreController : ControllerBase{
     {
         try
         {
-      string hello = "";
+            string hello = "";
             var engine = Python.CreateEngine(); // Extract Python language engine from their grasp
             var scope = engine.CreateScope(); // Introduce Python namespace (scope)
-      var d = new Dictionary<string, object>
-            {
-                { "hello", hello}
-            }; // Add some sample parameters. Notice that there is no need in specifically setting the object type, interpreter will do that part for us in the script properly with high probability
+            scope.SetVariable("hello", hello);
+            ScriptSource source = engine.CreateScriptSourceFromFile("../Scripts/hello.py"); // Load the script
+            object result = source.Execute(scope);
+            hello = scope.GetVariable<string>("hello");
 
-      return Ok();
+            return Ok(hello);
         }
         catch (Exception e)
         {
