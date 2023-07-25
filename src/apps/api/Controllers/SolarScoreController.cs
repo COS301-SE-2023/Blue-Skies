e57eaf20cf3823e0443f81d4cefa234c6297437a
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-07-25 09:57:03 +0200

    Set up AI model

---
+++ src/apps/api/Controllers/SolarScoreController.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Api.Repository;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SolarScoreController : ControllerBase
+{
+    [HttpGet]
+    [Route("test")]
+    public async Task<IActionResult> testImage()
+    {
+        //Load sample data
+        var imageBytes = System.IO.File.ReadAllBytes(
+            @"C:\Users\naido\Downloads\image-classifier-assets\assets\images\Broccoli\broccoli.jpg"
+        );
+        MLModel.ModelInput sampleData = new MLModel.ModelInput() { ImageSource = imageBytes, };
+
+        //Load model and predict output
+        var result = MLModel.Predict(sampleData);
+        Console.WriteLine(result);
+
+        return Ok(result);
+    }
+}
