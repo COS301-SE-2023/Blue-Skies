05993ae2425ad8e5062421e1361bdd1a0c708dd1
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-07-28 13:32:49 +0200

    Fixed errors

--- src/apps/api/Controllers/SolarScoreController.cs
+++ src/apps/api/Controllers/SolarScoreController.cs
@@ -19,23 +19,6 @@ public class SolarScoreController : ControllerBase
     }
 
     [HttpGet]
-    [Route("getSolarScore")]
-    public async Task<IActionResult> testImage([FromBody] Coordinates cord)
-    {
-        //Load sample data
-        var imageBytes = System.IO.File.ReadAllBytes(
-            @"C:\Users\naido\Downloads\image-classifier-assets\assets\images\Broccoli\broccoli.jpg"
-        );
-        MLModel.ModelInput sampleData = new MLModel.ModelInput() { ImageSource = imageBytes, };
-
-        //Load model and predict output
-        var result = MLModel.Predict(sampleData);
-        Console.WriteLine(result);
-
-        return Ok(result);
-    }
-
-    [HttpGet]
     [Route("mapboxkey")]
     public async Task<IActionResult> GetMapBoxKey()
     {
