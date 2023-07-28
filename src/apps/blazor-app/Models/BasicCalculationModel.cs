ca728239d4bb38235e4343986468013b878c6c3f
Jason <u21477452@tuks.co.za>
2023-07-28 16:00:56 +0200

    Added setup for integration in advanced calculation

---
+++ src/apps/blazor-app/Models/BasicCalculationModel.cs
@@ -0,0 +1,9 @@
+public class BasicCalculationModel
+{
+    public int? basicCalculationId { get; set; }
+    public int systemId { get; set; }
+    public int daylightHours { get; set; }
+    public string? location { get; set; }
+    public int batteryLife { get; set; }
+    public DateTime? dateCreated { get; set; }
+}
