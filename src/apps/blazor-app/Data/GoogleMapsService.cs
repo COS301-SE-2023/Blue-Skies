1f66c4bf82f0a759d52a4b7348ba439a1bbe8429
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-07-30 23:39:00 +0200

    Added map to Advanced calculations and info blob

--- src/apps/blazor-app/Data/GoogleMapsService.cs
+++ src/apps/blazor-app/Data/GoogleMapsService.cs
@@ -15,7 +15,7 @@ public class GoogleMapsService
     public async Task<byte[]> GetStaticMapImageAsync(double latitude, double longitude, int zoom, int width, int height)
     {
         Console.WriteLine($"Getting map image for {latitude}, {longitude}");
-        var url = $"https://maps.googleapis.com/maps/api/staticmap?center={longitude},{latitude}&zoom={zoom}&size={width}x{height}&maptype=satellite&key={apiKey}";
+        var url = $"https://maps.googleapis.com/maps/api/staticmap?center={longitude},{latitude}&zoom={zoom}&size={width}x{height}&maptype=satellite&markers=color:red|{latitude},{longitude}&key={apiKey}";
         return await _httpClient.GetByteArrayAsync(url);
     }
 }
