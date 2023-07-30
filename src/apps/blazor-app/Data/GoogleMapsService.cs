2cac88731f0c14e808ebc6939d25adf79feacc35
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-07-30 22:36:10 +0200

    Map integrated and added

---
+++ src/apps/blazor-app/Data/GoogleMapsService.cs
@@ -0,0 +1,21 @@
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class GoogleMapsService
+{
+    private readonly HttpClient _httpClient;
+    private readonly string apiKey = "";
+
+    public GoogleMapsService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<byte[]> GetStaticMapImageAsync(double latitude, double longitude, int zoom, int width, int height)
+    {
+        Console.WriteLine($"Getting map image for {latitude}, {longitude}");
+        var url = $"https://maps.googleapis.com/maps/api/staticmap?center={longitude},{latitude}&zoom={zoom}&size={width}x{height}&maptype=satellite&key={apiKey}";
+        return await _httpClient.GetByteArrayAsync(url);
+    }
+}
