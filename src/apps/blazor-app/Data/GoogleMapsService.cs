a6d365068e4ff9db81f5e05e9e77619b9f975f45
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-07-31 11:46:29 +0200

    Integrated google api key

--- src/apps/blazor-app/Data/GoogleMapsService.cs
+++ src/apps/blazor-app/Data/GoogleMapsService.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 public class GoogleMapsService
 {
     private readonly HttpClient _httpClient;
-    private readonly string apiKey = "";
+    private string apiKey = "";
+    private string? API_PORT = Environment.GetEnvironmentVariable("API_PORT");
 
     public GoogleMapsService(HttpClient httpClient)
     {
@@ -14,6 +15,14 @@ public class GoogleMapsService
 
     public async Task<byte[]> GetStaticMapImageAsync(double latitude, double longitude, int zoom, int width, int height)
     {
+        var request = new HttpRequestMessage(HttpMethod.Get, API_PORT + "/SolarScore/googlemapskey");
+        var response = await _httpClient.SendAsync(request);
+        if (response.StatusCode == System.Net.HttpStatusCode.OK){
+            apiKey = await response.Content.ReadAsStringAsync();
+        }
+        apiKey = apiKey.Trim('"');
+
+        Console.WriteLine(apiKey);
         Console.WriteLine($"Getting map image for {latitude}, {longitude}");
         var url = $"https://maps.googleapis.com/maps/api/staticmap?center={longitude},{latitude}&zoom={zoom}&size={width}x{height}&maptype=satellite&markers=color:red|{latitude},{longitude}&key={apiKey}";
         return await _httpClient.GetByteArrayAsync(url);
