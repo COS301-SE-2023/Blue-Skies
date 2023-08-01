eeb134d93c25edd58343e92172a7157464b0d0c1
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-08-01 10:51:28 +0200

    Successfully added new toasts

--- src/apps/blazor-app/Data/ToastService.cs
+++ src/apps/blazor-app/Data/ToastService.cs
@@ -3,10 +3,16 @@ namespace BlazorApp.Data {
     public class ToastService
     {
         public event Action<string, string>? ShowToastRequested;
+        public event Action? HideToastRequested;
 
         public void ShowToast(string message, string type)
         {
             ShowToastRequested?.Invoke(message, type);
+        }
+
+        public void HideToast()
+        {
+            HideToastRequested?.Invoke();
         }
     }
 
