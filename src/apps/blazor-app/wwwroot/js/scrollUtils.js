29838babd0dbc9bf81cddc9a7c558b21d224def4
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-08-05 00:18:30 +0200

    Generate pdf added

--- src/apps/blazor-app/wwwroot/js/scrollUtils.js
+++ src/apps/blazor-app/wwwroot/js/scrollUtils.js
@@ -1,9 +1,26 @@
-
 window.scrollUtils = {
-    getCurrentScrollPosition: function(dotnetHelper) {
-        window.onscroll = function () {
-            var scrollY = window.scrollY;
-            dotnetHelper.invokeMethodAsync('UpdateScrollPosition', scrollY);
-        };
-    }
+  getCurrentScrollPosition: function (dotnetHelper) {
+    window.onscroll = function () {
+      var scrollY = window.scrollY;
+      dotnetHelper.invokeMethodAsync('UpdateScrollPosition', scrollY);
+    };
+  },
+};
+
+window.GenerateReport = function () {
+  // Default export is a4 paper, portrait, using milimeters for units
+  var doc = new jsPDF();
+
+  doc.text('Hello world!', 10, 10);
+  //SAVE IMAGE
+  var img = new Image();
+  img.src = '../assets/Logo.svg';
+  doc.addImage(img, 'SVG', 10, 10, 100, 50);
+
+  // doc.text('This is client-side Javascript, pumping out a PDF.', 10, 10);
+  // doc.text('Do you like that?', 10, 20);
+
+  // // Save the PDF
+
+  doc.save('a4.pdf');
 };
