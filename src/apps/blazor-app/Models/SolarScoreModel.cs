581f541d841575afeab9b7b42898512ed422c538
Rukie007 <ruan@ufcons.com>
2023-08-04 20:35:02 +0200

    Revert "Revert "Revert "Merge branch 'development' into main"""
    This reverts commit 363bc827e85164a8882983a328f33aa17bcd4449.

--- src/apps/blazor-app/Models/SolarScoreModel.cs
+++ src/apps/blazor-app/Models/SolarScoreModel.cs
@@ -1,7 +1,7 @@
 public class SolarScoreModel
 {
     public string? imgName { get; set; }
-    public string? solarScoreId { get; set; }
+    public string solarScoreId { get; set; }
     public string? base64Image { get; set; }
 
     public string? score { get; set; }
