0f94dbbdb89473c9e56284ce4c927b876d2ec186
pseudo-jaycie <u21477452@tuks.co.za>
2023-08-05 22:57:38 +0200

    Added storing image to basic Calculation

--- src/apps/blazor-app/Data/GoogleMapsService.cs
+++ src/apps/blazor-app/Data/GoogleMapsService.cs
@@ -13,8 +13,13 @@ public class GoogleMapsService
         _httpClient = httpClient;
     }
 
-    public async Task<byte[]> GetStaticMapImageAsync(double longitude, double latitude, int zoom, int width, int height)
-
+    public async Task<byte[]> GetStaticMapImageAsync(
+        double longitude,
+        double latitude,
+        int zoom,
+        int width,
+        int height
+    )
     {
         var request = new HttpRequestMessage(
             HttpMethod.Get,
@@ -29,7 +34,7 @@ public class GoogleMapsService
 
         Console.WriteLine($"Getting map image for {latitude}, {longitude}");
         var url =
-            $"https://maps.googleapis.com/maps/api/staticmap?center={longitude},{latitude}&zoom={zoom}&size={width}x{height}&maptype=satellite&key={apiKey}";
+            $"https://maps.googleapis.com/maps/api/staticmap?center={longitude},{latitude}&zoom={zoom}&size={width}x{height}&maptype=satellite&markers=color:red|{latitude},{longitude}&key={apiKey}";
         return await _httpClient.GetByteArrayAsync(url);
     }
 }
