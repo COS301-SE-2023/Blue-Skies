15cd687a7987d82d7d57c669f5e4dc8e8cc925db
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-08-13 22:00:40 +0200

    get solar irradiance

---
+++ src/apps/blazor-app/Models/SolarIrradiationModel.cs
@@ -0,0 +1,9 @@
+public class SolarIrradiationModel
+{
+    public string? data { get; set; }
+    public int remainingCalls { get; set; }
+    public double latitude { get; set; }
+    public double longitude { get; set; }
+
+    public DateTime? dateCreated { get; set; }
+}
