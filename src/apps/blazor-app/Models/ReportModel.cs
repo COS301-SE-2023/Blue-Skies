b163a074cc67cec28e56f5db32add37f98d4c769
Jason <u21477452@tuks.co.za>
2023-08-13 22:00:47 +0200

    Added changes by removing basic calculation and report all

--- src/apps/blazor-app/Models/ReportModel.cs
+++ src/apps/blazor-app/Models/ReportModel.cs
@@ -5,7 +5,7 @@ public class ReportModel
     public int userId { get; set; }
     public int systemId { get; set; }
     public int locationId { get; set; }
-    public string? daylightHours { get; set; }
+    public float daylightHours { get; set; }
     public string? image { get; set; }
     public DateTime dateCreated { get; set; }
 }
