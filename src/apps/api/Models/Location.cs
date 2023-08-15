a4a20b37164720b5959de2d9ddf84dab59238e80
pseudo-jaycie <u21477452@tuks.co.za>
2023-08-15 13:37:51 +0200

    Updated Location API

--- src/apps/api/Models/Location.cs
+++ src/apps/api/Models/Location.cs
@@ -3,7 +3,6 @@ namespace Api;
 public class Locations
 {
     public int locationId { get; set; }
-    public string? location { get; set; }
     public float latitude { get; set; }
     public float longitude { get; set; }
 }
