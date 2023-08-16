afba2d9fa1da53308356b2a619992dee53f1f362
Jason <u21477452@tuks.co.za>
2023-08-16 19:41:55 +0200

    Solar Score page working - big dub

--- src/apps/blazor-app/Data/SolarScoreService.cs
+++ src/apps/blazor-app/Data/SolarScoreService.cs
@@ -1,42 +1,71 @@
 using System;
 using System.Text.Json;
-namespace BlazorApp.Data {
-    public class SolarCalculator {
+
+namespace BlazorApp.Data
+{
+    public class SolarCalculator
+    {
         private double perfectSolarIrradiation = 220;
-        public async Task<int[]> GetDataLocationData(double latitude, double longitude, string API_PORT, int solarScore, int remainingCalls)
+
+        public async Task<int[]> GetDataLocationData(
+            double latitude,
+            double longitude,
+            string API_PORT,
+            int solarScore,
+            int remainingCalls
+        )
         {
-            Console.WriteLine("I am calling GetDataLocationData");
-            int[] result = {solarScore, remainingCalls};
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)
+            System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            int[] result = { solarScore, remainingCalls };
             var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, API_PORT + "/locationData/getLocationDataWithoutImage/" + latitude + "/" + longitude);
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                API_PORT + "/locationData/getLocationDataWithoutImage/" + latitude + "/" + longitude
+            );
             var response = await client.SendAsync(request);
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 var data = await response.Content.ReadAsStringAsync();
-                if(data.Equals("Solar Irradiation not found")) {
+                if (data.Equals("Solar Irradiation not found"))
+                {
                     return result;
                 }
                 var locationData = JsonSerializer.Deserialize<LocationDataModel>(data);
-                if(locationData == null || locationData.data == null) {
+                if (locationData == null || locationData.data == null)
+                {
                     return result;
                 }
                 result[0] = calculateSolarScore(locationData.data);
                 result[1] = locationData.remainingCalls;
-                Console.WriteLine("Remaining calls: " + remainingCalls + " for call with latitude: " + latitude + " and longitude: " + longitude);
-            }else{
+                Console.WriteLine(
+                    "Remaining calls: "
+                        + remainingCalls
+                        + " for call with latitude: "
+                        + latitude
+                        + " and longitude: "
+                        + longitude
+                );
+            }
+            else
+            {
                 Console.WriteLine("Failed to get data from LocationData");
             }
-            Console.WriteLine("I am returning from GetSGetDataLocationDataolarIrradiation");
             return result;
         }
 
-        private int calculateSolarScore(string data) {
+        private int calculateSolarScore(string data)
+        {
             string input = data;
             double total = 0;
             int i = 0;
-            while(input.Length > 0) {
+            while (input.Length > 0)
+            {
                 int newDataPointIndex = input.IndexOf(",");
-                if(newDataPointIndex == -1) {
+                if (newDataPointIndex == -1)
+                {
                     newDataPointIndex = input.Length;
                 }
                 string newDataPoint = input.Substring(0, newDataPointIndex);
@@ -46,9 +75,10 @@ namespace BlazorApp.Data {
                 total += Double.Parse(newDataPoint.Substring(solarScoreIndex + 1));
                 i++;
             }
-            double averageSolarIrradiation = total /i;
-            double solarScore = averageSolarIrradiation/perfectSolarIrradiation * 100;
-            if(solarScore > 100) {
+            double averageSolarIrradiation = total / i;
+            double solarScore = averageSolarIrradiation / perfectSolarIrradiation * 100;
+            if (solarScore > 100)
+            {
                 solarScore = 100;
             }
             return (int)solarScore;
