8c17a517c6c04c425f23e5e025b370cd9fdc438a
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-08-21 08:10:15 +0200

    Added toast timeout and progress bar && Fixed Dashboard whitespace

--- src/apps/blazor-app/Data/ToastService.cs
+++ src/apps/blazor-app/Data/ToastService.cs
@@ -5,9 +5,11 @@ namespace BlazorApp.Data {
         public event Action<string, string, string>? ShowToastRequested;
         public event Action? HideToastRequested;
 
-        public void ShowToast(string title, string message, string type)
+        public async void ShowToast(string title, string message, string type)
         {
             ShowToastRequested?.Invoke(title, message, type);
+            await Task.Delay(4000);
+            HideToast();
         }
 
         public void HideToast()
