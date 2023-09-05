e818fdcf4d990f4849438093959f8639ed3e2bb9
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-09-05 11:11:03 +0200

    Added Tutorial modal

--- src/apps/blazor-app/Data/TutorialService.cs
+++ src/apps/blazor-app/Data/TutorialService.cs
@@ -1,17 +1,13 @@
 namespace BlazorApp.Data {
     public class TutorialService {
-        public event Action<string, string, string>? ShowTutorialRequested;
-        public event Action? HideTutorialRequested;
+        public event Action ShowTutorialRequested;
+        public event Action HideTutorialRequested;
 
-        public async void ShowTutorial(string title, string message, string type)
-        {
-            ShowTutorialRequested?.Invoke(title, message, type);
-            await Task.Delay(6000);
-            HideTutorial();
+        public void ShowTutorial() {
+            ShowTutorialRequested?.Invoke();
         }
 
-        public void HideTutorial()
-        {
+        public void HideTutorial() {
             HideTutorialRequested?.Invoke();
         }
     }
