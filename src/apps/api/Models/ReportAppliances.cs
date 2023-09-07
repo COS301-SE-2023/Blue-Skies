b9f9712a77792f228a74e6c39009deafbca83d13
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-09-07 13:38:50 +0200

    Create report appliance refactored

--- src/apps/api/Models/ReportAppliances.cs
+++ src/apps/api/Models/ReportAppliances.cs
@@ -5,4 +5,8 @@ public class ReportAppliances
     public int reportId { get; set; }
     public int applianceId { get; set; }
     public int numberOfAppliances { get; set; }
+    public string applianceModel { get; set; }
+    public int powerUsage { get; set; }
+    public float durationUsed { get; set; }
+
 }
