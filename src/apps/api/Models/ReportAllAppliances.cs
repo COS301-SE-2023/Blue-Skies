89cbe7fa53a52733995984e58758d581899dc89d
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-09-12 13:14:22 +0200

    reportAllAppliances refactored

--- src/apps/api/Models/ReportAllAppliances.cs
+++ src/apps/api/Models/ReportAllAppliances.cs
@@ -6,5 +6,15 @@ public class ReportAllAppliance
     public int applianceId { get; set; }
     public int numberOfAppliances { get; set; }
     public string? type { get; set; }
-    public int powerUsage { get; set; }
+    public string? applianceModel { get; set; }
+    public int? powerUsage { get; set; }
+    // durationUsed
+    public float? durationUsed { get; set; }
+    // defaultPowerUsage
+    public int defaultPowerUsage { get; set; }
+    // defaultDurationUsed
+    public float defaultDurationUsed { get; set; }
+
+
+
 }
