e87458bb401212d7f9574a6c15335dd23765da38
Rukie007 <ruan@ufcons.com>
2023-09-13 23:06:41 +0200

    Update LocationDataModel.cs

--- src/libs/shared-utils/models/LocationDataModel.cs
+++ src/libs/shared-utils/models/LocationDataModel.cs
@@ -3,6 +3,7 @@ public class LocationDataModel
     public double latitude { get; set; }
     public double longitude { get; set; }
     public string? locationName { get; set; }
+    public string? solarPanelsData { get; set; }
     public byte[]? satteliteImageData { get; set; }
     public byte[]? satteliteImageElevationData { get; set; }
     public byte[]? annualFluxData { get; set; }
