99459d69797914b3d61c4ad4c8c94a80a0fb3a0e
Rukie007 <ruan@ufcons.com>
2023-09-14 15:31:58 +0200

    Removed bunch of warnings

--- src/libs/shared-utils/models/RooftopInformationModel.cs
+++ src/libs/shared-utils/models/RooftopInformationModel.cs
@@ -1,14 +1,14 @@
 
 public class RooftopInformationModel
 {
-  public string name { get; set; }
-  public Center center { get; set; }
-  public Imagerydate imageryDate { get; set; }
-  public string regionCode { get; set; }
-  public Solarpotential solarPotential { get; set; }
-  public Boundingbox1 boundingBox { get; set; }
-  public string imageryQuality { get; set; }
-  public Imageryprocesseddate imageryProcessedDate { get; set; }
+  public string? name { get; set; }
+  public Center? center { get; set; }
+  public Imagerydate? imageryDate { get; set; }
+  public string? regionCode { get; set; }
+  public Solarpotential? solarPotential { get; set; }
+  public Boundingbox1? boundingBox { get; set; }
+  public string? imageryQuality { get; set; }
+  public Imageryprocesseddate? imageryProcessedDate { get; set; }
 }
 
 public class Center
@@ -30,28 +30,28 @@ public class Solarpotential
   public float maxArrayAreaMeters2 { get; set; }
   public float maxSunshineHoursPerYear { get; set; }
   public float carbonOffsetFactorKgPerMwh { get; set; }
-  public Wholeroofstats wholeRoofStats { get; set; }
-  public Roofsegmentstat[] roofSegmentStats { get; set; }
-  public Solarpanelconfig[] solarPanelConfigs { get; set; }
+  public Wholeroofstats? wholeRoofStats { get; set; }
+  public Roofsegmentstat[]? roofSegmentStats { get; set; }
+  public Solarpanelconfig[]? solarPanelConfigs { get; set; }
   public int panelCapacityWatts { get; set; }
   public float panelHeightMeters { get; set; }
   public float panelWidthMeters { get; set; }
   public int panelLifetimeYears { get; set; }
-  public Buildingstats buildingStats { get; set; }
-  public Solarpanel[] solarPanels { get; set; }
+  public Buildingstats? buildingStats { get; set; }
+  public Solarpanel[]? solarPanels { get; set; }
 }
 
 public class Wholeroofstats
 {
   public float areaMeters2 { get; set; }
-  public float[] sunshineQuantiles { get; set; }
+  public float[]? sunshineQuantiles { get; set; }
   public float groundAreaMeters2 { get; set; }
 }
 
 public class Buildingstats
 {
   public float areaMeters2 { get; set; }
-  public float[] sunshineQuantiles { get; set; }
+  public float[]? sunshineQuantiles { get; set; }
   public float groundAreaMeters2 { get; set; }
 }
 
@@ -59,16 +59,16 @@ public class Roofsegmentstat
 {
   public float pitchDegrees { get; set; }
   public float azimuthDegrees { get; set; }
-  public Stats stats { get; set; }
-  public Center1 center { get; set; }
-  public Boundingbox boundingBox { get; set; }
+  public Stats? stats { get; set; }
+  public Center1? center { get; set; }
+  public Boundingbox? boundingBox { get; set; }
   public float planeHeightAtCenterMeters { get; set; }
 }
 
 public class Stats
 {
   public float areaMeters2 { get; set; }
-  public float[] sunshineQuantiles { get; set; }
+  public float[]? sunshineQuantiles { get; set; }
   public float groundAreaMeters2 { get; set; }
 }
 
@@ -80,8 +80,8 @@ public class Center1
 
 public class Boundingbox
 {
-  public Sw sw { get; set; }
-  public Ne ne { get; set; }
+  public Sw? sw { get; set; }
+  public Ne? ne { get; set; }
 }
 
 public class Sw
@@ -100,7 +100,7 @@ public class Solarpanelconfig
 {
   public int panelsCount { get; set; }
   public float yearlyEnergyDcKwh { get; set; }
-  public Roofsegmentsummary[] roofSegmentSummaries { get; set; }
+  public Roofsegmentsummary[]? roofSegmentSummaries { get; set; }
 }
 
 public class Roofsegmentsummary
@@ -114,8 +114,8 @@ public class Roofsegmentsummary
 
 public class Solarpanel
 {
-  public Center2 center { get; set; }
-  public string orientation { get; set; }
+  public Center2? center { get; set; }
+  public string? orientation { get; set; }
   public float yearlyEnergyDcKwh { get; set; }
   public int segmentIndex { get; set; }
 }
@@ -128,8 +128,8 @@ public class Center2
 
 public class Boundingbox1
 {
-  public Sw1 sw { get; set; }
-  public Ne1 ne { get; set; }
+  public Sw1? sw { get; set; }
+  public Ne1? ne { get; set; }
 }
 
 public class Sw1
