2532346f70d0c39196740006f34183b2b6156b9d
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-09-22 14:29:00 +0200

    Fixed appliance quantity issue

--- src/apps/blazor-app/Data/ApplianceService.cs
+++ src/apps/blazor-app/Data/ApplianceService.cs
@@ -3,12 +3,19 @@ namespace BlazorApp.Data
     public class ApplianceService
     {
         public event Action<List<ApplianceModel>>? UpdateAppliancesRequested;
+        public event Action<List<ApplianceModel>>? EmmitAppliancesRequested;
 
-        public event Action ClearSearchRequested;
+        public event Action? ClearSearchRequested;
+
 
         public void UpdateAppliance(List<ApplianceModel> appliances)
         {
             UpdateAppliancesRequested?.Invoke(appliances);
+        }
+
+        public void EmmitAppliances(List<ApplianceModel> appliances)
+        {
+            EmmitAppliancesRequested?.Invoke(appliances);
         }
 
         public void ClearSearch()
