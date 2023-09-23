c17d0ae17e953570609d4116efc195375abc1dc1
Jesse Naidoo <jessenaidoo24@gmail.com>
2023-09-23 23:17:44 +0200

    Removed warnings

--- src/apps/api/Models/ReportAllAppliances.cs
+++ src/apps/api/Models/ReportAllAppliances.cs
@@ -7,9 +7,9 @@ public class ReportAllAppliance
     public int numberOfAppliances { get; set; }
     public string? type { get; set; }
     public string? applianceModel { get; set; }
-    public int? powerUsage { get; set; }
+    public int powerUsage { get; set; }
     // durationUsed
-    public float? durationUsed { get; set; }
+    public float durationUsed { get; set; }
     // defaultPowerUsage
     public int defaultPowerUsage { get; set; }
     // defaultDurationUsed
