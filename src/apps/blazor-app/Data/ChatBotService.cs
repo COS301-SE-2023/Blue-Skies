aedee8ccc7c8274c7994b984e108894dda419bc7
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-09-28 04:45:42 +0200

    Fixed Merge Commits

---
+++ src/apps/blazor-app/Data/ChatBotService.cs
@@ -0,0 +1,18 @@
+namespace BlazorApp.Data {
+    public class ChatBotService
+    {
+        public event Action? ShowChatBotRequested;
+        public event Action? HideChatBotRequested;
+
+        public void ShowChatBot()
+        {
+            ShowChatBotRequested?.Invoke();
+        }
+
+        public void HideChatBot()
+        {
+            HideChatBotRequested?.Invoke();
+        }
+    }
+
+}
