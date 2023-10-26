295b214c5f9e610dcf3ed3d72ea198cc494375d0
jason-dutton <u21477452@tuks.co.za>
2023-10-26 02:39:20 +0200

    swapped legend items

--- src/apps/blazor-app/Data/Tooltips.cs
+++ src/apps/blazor-app/Data/Tooltips.cs
@@ -8,7 +8,8 @@ namespace BlazorApp.Data {
             tooltips["solarScore"] = "The percentage of sunlight exposure on your property's roof compared to an optimal roof.";
             tooltips["potential savings"] = "How much money you can save monthly by producing clean energy.";
             tooltips["average energy produced"] = "The daily energy your solar panels produce averaged over a year.";
-            tooltips["running hours on batteries"] = "The total time in which you can run the chosen appliances exclusively using battery power.";
+            tooltips["running hours on batteries"] = "The average time your batteries will last given daily duration usage of your appliances";
+            tooltips["simultaneous running hours"] = "The time your batteries will last given simultaneous and constant use of all your appliances";
             tooltips["battery utilisation"] = "The maximum percentage your batteries can charge, while running the selected appliances.";
             tooltips["solar score add button"] = "Add or remove appliances";
             tooltips["number of panels"] = "Affects your average energy production and how quickly you can charge your batteries while running appliances.";
